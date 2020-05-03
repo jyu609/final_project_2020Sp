16f40ac51c36ff32cac9a0af7a51c3444e22d446
Yilin Tan <yilint2@illinois.edu>
2020-05-02 21:42:53 -0500

    Add covariance and correlation coefficience functions

--- final_project.py
+++ final_project.py
@@ -1,5 +1,42 @@
+import pandas as pd
+import numpy as np
+
+
 def correlation_analysis():
     print("start analysis")
+
+
+def calculate_covariance(column1: pd.Series, column2: pd.Series) -> np.float64:
+    """
+    calcualte the covariance of the two dataframe columns
+    :param column1: one column of a dataframe
+    :param column2: one column of a dataframe
+    :return covariance of the two input columns
+
+    >>> test_df = pd.DataFrame({"A":[1,2,3,4], "B": [5,6,7,8]})
+    >>> calculate_covariance(test_df.A, test_df.B)
+    1.6666666666666665
+
+    """
+
+    cov = column1.cov(column2)
+    return cov
+
+def calculate_correlation_coefficience(column1: pd.Series, column2: pd.Series) -> np.float64:
+    """
+    calcualte the correlation coefficience of the two dataframe columns
+    :param column1: one column of a dataframe
+    :param column2: one column of a dataframe
+    :return correlation coefficience of the two input columns
+
+    >>> test_df = pd.DataFrame({"A":[1,2,3,4], "B": [5,6,7,8]})
+    >>> calculate_correlation_coefficience(test_df.A, test_df.B)
+    1.0
+
+    """
+
+    corr = column1.corr(column2)
+    return corr
 
 if __name__ == '__main__':
     correlation_analysis()
